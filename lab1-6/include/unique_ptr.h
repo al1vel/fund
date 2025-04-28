ca9f2f57a36f31a2e687528be777b3571096c07d
begemot <m1233danil@bk.ru>
2025-04-29 01:19:14 +0300

    lab6: created structure.

---
+++ lab1-6/include/unique_ptr.h
@@ -0,0 +1,8 @@
+#ifndef UNIQUE_PTR_H
+#define UNIQUE_PTR_H
+
+namespace smart_ptr {
+    template<class>
+}
+
+#endif //UNIQUE_PTR_H
---
+++ lab1-6/src/main.cpp
@@ -0,0 +1,6 @@
+#include "unique_ptr.h"
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+}
